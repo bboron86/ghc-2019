d7f2f845892815fb12f1da48f5a6e8ae46a4e99d
bartoszb <bartosz.boron@maibornwolff.de>
2019-02-28 19:48:06 +0100

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
 import cityplan.CityBuilder
+import cityplan.splitByBlank
 import java.io.File
 
-private const val FILE = "src/main/resources/b_lovely_landscapes"
+private const val FILE = "src/main/resources/a_example"
 
 
 fun main(args: Array<String>) {
-//    CityBuilder().build()
 
-    File("$FILE.txt").useLines {
+    listOf(
+        "a_example",
+        "c_memorable_moments"
+    ).forEach { processFile(it) }
+}
+
+private fun processFile(currentFile: String) {
+    File("src/main/resources/$currentFile.txt").useLines {
         val inputList = it.toList()
 
         // index 0 = number photos
@@ -17,44 +24,51 @@ fun main(args: Array<String>) {
         val vPhotos = mutableListOf<VerticalPhoto>()
 
         inputList.forEachIndexed { index, line ->
+            if (index == 0) return@forEachIndexed
+
+            val tags = line.splitByBlank().filterIndexed { id, s -> id > 1 }
+
             if (line.startsWith('H')) {
-                hPhotos.add(HorizontalPhoto(index - 1))
+                hPhotos.add(HorizontalPhoto(index - 1, tags))
             } else if (line.startsWith('V')) {
-                vPhotos.add(VerticalPhoto(index - 1))
+                vPhotos.add(VerticalPhoto(index - 1, tags))
             }
         }
 
+        println(hPhotos)
+        println(vPhotos)
+
+        /******************* SLIDE SHOW CREATION ************************************/
         val slideShow = mutableListOf<Slide>()
-        hPhotos.forEach { hPhoto -> slideShow.add(Slide(listOf(hPhoto.id))) }
+        hPhotos.forEach { hPhoto ->
+            slideShow.add(Slide(listOf(hPhoto.id), hPhoto.tags.toSet()))
+        }
+
         vPhotos.chunked(2).forEach { chunk ->
             if (chunk.size == 2) {
-                slideShow.add(Slide(listOf(chunk[0].id, chunk[1].id)))
+                slideShow.add(Slide(listOf(chunk[0].id, chunk[1].id), chunk[0].tags.toSet() + chunk[1].tags.toSet()))
             }
         }
 
-//        println(hPhotos)
-//        println(vPhotos)
         println(slideShow)
 
-//        File("$FILE.out.txt").createNewFile()
-//        val outPutFile = File("$FILE.out.txt").writer()
-//        outPutFile.write(slideShow.size)
-//        outPutFile.flush()
-        File("$FILE.out.txt").delete()
-        File("$FILE.out.txt").appendWithNewLine(slideShow.size.toString())
+
+        /********************** OUTPUT GENERATION ***********************************/
+        File("src/main/resources/$currentFile.out.txt").delete()
+        File("src/main/resources/$currentFile.out.txt").appendWithNewLine(slideShow.size.toString())
         slideShow.forEach { slide ->
-            if (slide.photos.size == 2)
-                File("$FILE.out.txt").appendWithNewLine("${slide.photos[0]} ${slide.photos[1]}")
+            if (slide.photoIds.size == 2)
+                File("src/main/resources/$currentFile.out.txt").appendWithNewLine("${slide.photoIds[0]} ${slide.photoIds[1]}")
             else
-                File("$FILE.out.txt").appendWithNewLine("${slide.photos[0]}")
+                File("src/main/resources/$currentFile.out.txt").appendWithNewLine("${slide.photoIds[0]}")
         }
     }
 }
 
-data class HorizontalPhoto(val id: Int)
+data class HorizontalPhoto(val id: Int, val tags: List<String>)
 
-data class VerticalPhoto(val id: Int)
+data class VerticalPhoto(val id: Int, val tags: List<String>)
 
-data class Slide(val photos: List<Int>)
+data class Slide(val photoIds: List<Int>, val tags: Set<String>)
 
 fun File.appendWithNewLine(text: String) = this.appendText("$text\n")
